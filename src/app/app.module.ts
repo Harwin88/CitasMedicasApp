c80c5c2726a8887a1c6c0132eba9ee77c5de99ea
harwin <harwingalvis88@gmail.com>
2020-06-14 21:12:50 -0500

    add buscador y filtros.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatTooltipModule} from '@angular/material/tooltip';
 import {MatIconModule} from '@angular/material/icon';
 import {MatListModule} from '@angular/material/list';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   exports:[
@@ -35,7 +36,8 @@ import {MatListModule} from '@angular/material/list';
     NgxMasonryModule,
     MatTooltipModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
